e84de985fa9156a78692b65f18b70f59334431d1
iMaary <m.rodriguesnave@gmail.com>
2019-08-28 12:16:50 -0300

    Movimentação básica implementada

---
+++ Assets/Scripts/MovimentBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float moveHoriz, moveVert;
+    [SerializeField] private Joystick joystick;
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        moveHoriz = joystick.Horizontal * speed;
+        moveVert = joystick.Vertical * speed;
+
+        transform.Translate(Vector3.right * moveHoriz * Time.deltaTime);
+        transform.Translate(Vector3.up * moveVert * Time.deltaTime);
+
+        moveHoriz = (joystick.Horizontal >= 0.3f) ? speed: (joystick.Horizontal <= -0.3f)? -speed : 0f;
+        moveVert = (joystick.Horizontal >= 0.3f) ? speed : (joystick.Horizontal <= -0.3f) ? -speed : 0f;
+    }
+}
